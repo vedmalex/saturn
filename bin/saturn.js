6d93b59ccad6f6bfb83376b3842b07a94aba7790
Tom Coleman <tom@thesnail.org>
2016-06-08 17:26:17 +1000

    Used concurrently in a very hacky way to get `saturn dev`

--- bin/saturn.js
+++ bin/saturn.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 
-var command = process.argv[2];
-var file = process.argv[3];
+var argv = require('yargs').argv;
+
+var command = argv._.shift();
+
 var path = require('path');
-var pathToFile = path.join(process.cwd(), file);
+// Map remaining arguments to paths inside the app
+var files = argv._.map(function(file) {
+  return path.join(process.cwd(), file);
+});
 
 require('../babel/server.babel'); // babel registration (runtime transpilation for node)
 require('dotenv').config({ silent: true });
 
-if (command === 'watch-client') {
-  require('../server/webpack-dev')(pathToFile);
+if (command === 'dev') {
+  var spawn = require('child_process').spawn;
+
+  var saturn = path.resolve(__dirname, './saturn.js');
+  var startDev = saturn + ' start-dev ' + argv.appServer;
+  var startDevApi = saturn + ' start-dev-api ' + argv.apiServer;
+  var watchClient = saturn + ' watch-client ' + argv.appClient;
+
+  var concurrently = path.resolve(__dirname, '../vendor/concurrently.js');
+  var child = spawn(concurrently, ['--kill-others', startDev, startDevApi, watchClient]);
+  child.stdout.on('data', function(d) { console.log(d.toString()); });
+  child.stderr.on('data', function(d) { console.err(d.toString()); });
+
+} else if (command === 'watch-client') {
+  require('../server/webpack-dev')(files[0]);
 } else {
   if (process.env.NODE_ENV !== 'production') {
     if (require('piping')({
@@ -38,10 +56,10 @@ if (command === 'watch-client') {
       global.webpackIsomorphicTools = new WebpackIsomorphicTools(require('../webpack/webpack-isomorphic-tools'))
         .development(__DEVELOPMENT__)
         .server(path.resolve(__dirname, '..'), function() {
-          require(pathToFile);
+          require(files[0]);
         });
     } else if (command === 'start-dev-api') {
-      require(pathToFile);
+      require(files[0]);
     }
   }
 }
