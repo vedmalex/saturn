013639912b0a85bda9ae6a3ed357ff250d9a8381
Tom Coleman <tom@thesnail.org>
2016-06-22 17:05:30 +1000

    Move passport usage out of saturn
    It seems the app needs to do this (or the order was wrong).

--- server/api.js
+++ server/api.js
@@ -1,22 +1,12 @@
 import express from 'express';
 import session from 'express-session';
-import passport from 'passport';
 import { apolloServer } from 'apollo-server';
 import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-// XXX: is this app specific?
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
 
 import config from '../config';
 app.start = () => {
