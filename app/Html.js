71bc6bff4efe7b7890f01b22a8a581e619586491
Tom Coleman <tom@thesnail.org>
2016-06-27 19:28:37 +1000

    Remove bootstrap gunk

--- app/Html.js
+++ app/Html.js
@@ -36,19 +36,12 @@ export default class Html extends Component {
 
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+
           {/* styles (will be present only in production with webpack extract text plugin) */}
           {Object.keys(assets.styles).map((style, key) =>
             <link href={assets.styles[style]} key={key} media="screen, projection"
                   rel="stylesheet" type="text/css" charSet="UTF-8"/>
           )}
-
-          {/* (will be present only in development mode) */}
-          {/* outputs a <style/> tag with all bootstrap styles + App.scss + it could be CurrentPage.scss. */}
-          {/* can smoothen the initial style flash (flicker) on page load in development mode. */}
-          {/* ideally one could also include here the style for the current page (Home.scss, About.scss, etc) */}
-          {/* Object.keys(assets.styles).length === 0 ? <style dangerouslySetInnerHTML={{__html: require(require('path').resolve(process.cwd(), './ui/style.css'))._style}}/> : null */}
-          {/* XXX: get rid of this */}
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous"/>
         </head>
         <body>
           <div id="content" dangerouslySetInnerHTML={{__html: content}}/>
