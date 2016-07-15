9d13d88e07a3d74c2397f389b4edb0e8b0bebdb9
Tom Coleman <tom@thesnail.org>
2016-07-15 10:26:36 +1000

    Whoa, who put that there :)

--- app/apollo-client.js
+++ app/apollo-client.js
@@ -10,7 +10,7 @@ let url;
 let options = {};
 if (__SERVER__) {
   options.ssrMode = true;
-  url = `http://#{config.host}:#{config.port}/graphql`;
+  url = `http://${config.host}:${config.port}/graphql`;
 } else {
   options.ssrForceFetchDelay = 100;
   url = '/graphql'
